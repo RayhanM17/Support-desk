dec91308d6a6a514607cc7a75e91b29fda28076c
RayhanM17 <raysdigitalservice@gmail.com>
2022-10-09 16:56:33 -0400

    notes back end user

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
 import { toast } from 'react-toastify'
 import {useSelector, useDispatch} from 'react-redux'
-import {getTicket, reset} from '../features/tickets/ticketSlice'
-import {useParams} from 'react-router-dom'
+import {getTicket, reset, closeTicket} from '../features/tickets/ticketSlice'
+import {useParams, useNavigate} from 'react-router-dom'
 import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 
@@ -11,6 +11,7 @@ function Ticket() {
 
   const dispatch = useDispatch()
   const {ticketId} = useParams()
+  const navigate = useNavigate()
 
   useEffect(() => {
     if(isError) {
@@ -20,6 +21,13 @@ function Ticket() {
     dispatch(getTicket(ticketId))
     // eslint-disable-next-line
   }, [isError, message, ticketId])
+
+  // Close Ticket
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId))
+    toast.success('Ticket Closed')
+    navigate('/tickets')
+  }
 
   if(isLoading) {
     return <Spinner />
@@ -31,19 +39,24 @@ function Ticket() {
   return <div className="ticket-page">
     <header className="ticket-header">
       <BackButton url='/tickets'/>
-    </header>
-    <h2>
+      <h2>
       Ticket ID: {ticket._id}
       <span className={`status status-${ticket.status}`}>
         {ticket.status}
       </span>
-    </h2>
-    <h3>Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}</h3>
-    <hr />
-    <div className="ticket-desc">
-      <h3>Description of Issue</h3>
-      <p>{ticket.description}</p>
-    </div>
+      </h2>
+      <h3>Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}</h3>
+      <h3>Product: {ticket.product}</h3>
+      <hr />
+      <div className="ticket-desc">
+        <h3>Description of Issue</h3>
+        <p>{ticket.description}</p>
+      </div>
+    </header>
+
+    {ticket.status !== 'closed' && (
+      <button onClick={onTicketClose} className='btn btn-block btn-danger'>Close Ticket</button>
+    )}
   </div>
 }
 
